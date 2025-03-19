f135e3b970a170fde5beaf3c9c6fdd8a0d5104a1
Ebenezer <ebenkouakou7@gmail.com>
2025-03-19 10:08:44 +0200

    Add: Initial framework for the main application window

--- src/main/java/accommodationfinder/MainApp.java
+++ src/main/java/accommodationfinder/MainApp.java
@@ -1,4 +1,39 @@
 package accommodationfinder;
 
+import accommodationfinder.ui.MainWindow;
+import javax.swing.*;
+
 public class MainApp {
+
+    public static void main(String[] args) {
+
+
+        // Set Look and Feel using Nimbus L&F
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Nimbus Look and Feel not available, using default.");
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            try{
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.setVisible(true);
+
+            } catch (Exception e) {
+                System.err.println("Error creating main window: " + e.getMessage());
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Error starting the application",
+                        "Application Error", JOptionPane.ERROR_MESSAGE);
+
+
+
+            }
+        });
+    }
 }
---
+++ src/main/java/accommodationfinder/ui/MainWindow.java
@@ -0,0 +1,20 @@
+package accommodationfinder.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        setTitle("Student Accommodation Finder");
+        setSize(900, 900);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel titleLabel = new JLabel("Welcome to Res Finder!", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        add(titleLabel, BorderLayout.NORTH);
+
+        setLocationRelativeTo(null);
+    }
+
+}
