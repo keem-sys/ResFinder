426711c6cd7d22ec74bc129275dad250d505877c
Ebenezer <ebenkouakou7@gmail.com>
2025-03-19 12:42:56 +0200

    Add:  comments to user dao

--- src/main/java/accommodationfinder/data/DatabaseConnection.java
+++ src/main/java/accommodationfinder/data/DatabaseConnection.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     // Path should be changed after Term 3 to jdbc:h2:./student_accommodation_db for a persistent storage
+    // Also change it in the IntelliJ H2 console
     private static final String JDBC_URL =  "jdbc:h2:mem:accommodationfinder";
 
     // Get JDBC connection and trows SQL Exception if unsuccessful
