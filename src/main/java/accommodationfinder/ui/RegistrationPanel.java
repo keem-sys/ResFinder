ffe8a4eaa6eff6040b33b2b0a0ef106d7d1cbaf0
Ebenezer <ebenkouakou7@gmail.com>
2025-03-20 10:57:29 +0200

    Add: Basic input validation

--- src/main/java/accommodationfinder/ui/RegistrationPanel.java
+++ src/main/java/accommodationfinder/ui/RegistrationPanel.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrationPanel extends JPanel {
     private JLabel fullNameLbl, usernameLbl, emailLbl, passwordLbl, confirmPasswordLbl, errorMsgLbl;
@@ -33,10 +35,15 @@ public class RegistrationPanel extends JPanel {
         cancelButton = new JButton("Cancel");
 
         add(fullNameLbl);
+        add(fullNameField);
         add(usernameLbl);
+        add(usernameField);
         add(emailLbl);
+        add(emailField);
         add(passwordLbl);
+        add(passwordField);
         add(confirmPasswordLbl);
+        add(confirmPasswordField);
         add(errorMsgLbl);
         add(new JLabel());
         add(registerButton);
@@ -62,11 +69,41 @@ public class RegistrationPanel extends JPanel {
                 System.out.println("Password: " + password);
                 System.out.println("Confirm Password: " + confirmPassword);
 
+
+                // Registration logic
+                // Input validation
+                if (fullName.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty()
+                || confirmPassword.isEmpty()) {
+                    setErrorMessage("All fields are required");
+                    return;
+                }
+
+                // Email validation
+                String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+                Pattern pattern = Pattern.compile(emailRegex);
+                Matcher matcher = pattern.matcher(email);
+                if (!matcher.matches()) {
+                    setErrorMessage("Invalid email format.");
+                    return;
+                }
+
+                if (!password.equals(confirmPassword)) {
+                    setErrorMessage("Password do not match");
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    setErrorMessage("Password must be at least 8 characters long");
+                }
+
+                System.out.println("Full Name: " + fullName);
+                System.out.println("Username: " + username);
+                System.out.println("Email: " + email);
+
+
                 // Clear password fields after processing
                 passwordField.setText("");
                 confirmPasswordField.setText("");
-
-                // Important: implement actual registration logic and validation here in later steps
             }
         });
 
