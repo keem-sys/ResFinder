cffdf55c34457007e0fe5e11150ef7cbd307d64c
AidenWallace <wallaceaiden64@gmail.com>
2025-03-24 00:53:53 +0200

    Added test files

---
+++ src/test/java/accommodationfinder/data/DatabaseConnectionTest.java
@@ -0,0 +1,12 @@
+package accommodationfinder.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseConnectionTest {
+
+    @Test
+    void getConnection() {
+    }
+}
---
+++ src/test/java/accommodationfinder/ui/LoginPanelTest.java
@@ -0,0 +1,16 @@
+package accommodationfinder.ui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginPanelTest {
+
+    @Test
+    void getLoginPanel() {
+    }
+
+    @Test
+    void setErrorMessage() {
+    }
+}
